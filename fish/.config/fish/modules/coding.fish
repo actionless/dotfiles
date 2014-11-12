3f2beb4aa0d60116681718ce39a1a2617838125d
actionless <actionless.loveless@gmail.com>
2014-11-12 15:35:51 +0100

    feat(fish): added docker clean aliases

--- fish/.config/fish/modules/coding.fish
+++ fish/.config/fish/modules/coding.fish
@@ -6,3 +6,6 @@ alias_w a_grep "ack --ignore-dir=env "
 function a_replace -a grep_line original_value replace_value -d "replace stuff usinf ack"
 	a_grep -l --print0 "$grep_line" | xargs -0 -n 1 sed -i -e 's/'"$original_value"'/'"$replace_value"'/g'
 end
+
+alias_w docker_rm "docker rm -f (docker ps -a -q)"
+alias_w docker_rmi "docker rmi (docker images | grep '^<none>' | sed -r 's/[   ]+/ /g' | cut -d' ' -f3)"
--- fish/.config/fish/modules/git.fish
+++ fish/.config/fish/modules/git.fish
@@ -5,4 +5,3 @@ make_completion git_status_pwd "git status"
 function git_status_pwd
 	command git status $argv | grep -v "\.\."
 end
-
