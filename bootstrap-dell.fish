eeca51745f2238a70cd290e32ebe381ae6f44564
actionless <actionless.loveless@gmail.com>
2014-12-26 12:27:26 +0100

    fix(scripts: dell): recreate symlinks after copying gtk configs

--- bootstrap-dell.fish
+++ bootstrap-dell.fish
@@ -1,3 +1,4 @@
+#!/bin/fish
 for i in (ls | grep -v -e theme-tp -e \.md -e \.fish);
 	stow $i; or echo "failed: $i"
 end
--- fix-lxappearance-dell.fish
+++ fix-lxappearance-dell.fish
@@ -1,4 +1,5 @@
 #!/bin/fish
 mv ~/.gtkrc-2.0 theme-dell/.gtkrc-2.0 ;
 and  mv ~/.config/gtk-3.0/settings.ini theme-dell/.config/gtk-3.0/ ; 
-and echo "done"
+and echo "done" ;
+and ./bootstrap-dell.fish
