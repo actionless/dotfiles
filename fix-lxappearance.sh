413ef3fcb0b28ce508718a8310858eb06e823d9a
actionless <actionless.loveless@gmail.com>
2015-01-10 14:48:34 +0100

    fix(scripts: fix-lx): recreate links after

--- fix-lxappearance.sh
+++ fix-lxappearance.sh
@@ -3,6 +3,5 @@ SCRIPT_DIR=$(dirname $(readlink -e "$0"))
 THEME_DIR=$SCRIPT_DIR/$($SCRIPT_DIR/current_theme.sh)
 mv ~/.gtkrc-2.0 $THEME_DIR/.gtkrc-2.0 ;
 mv ~/.config/gtk-3.0/settings.ini $THEME_DIR/.config/gtk-3.0/ ;
+./bootstrap-dell.fish &&
 echo "done"
-#&&
-#./bootstrap-dell.fish &&
