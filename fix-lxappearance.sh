f2be0a7cadf8f5a76d8e3c8c1624229f6b63ad03
actionless <actionless.loveless@gmail.com>
2015-01-10 14:37:06 +0100

    scripts: generalized fix-lxappearance script

---
+++ fix-lxappearance.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+SCRIPT_DIR=$(dirname $(readlink -e "$0"))
+THEME_DIR=$SCRIPT_DIR/$($SCRIPT_DIR/current_theme.sh)
+mv ~/.gtkrc-2.0 $THEME_DIR/.gtkrc-2.0 ;
+mv ~/.config/gtk-3.0/settings.ini $THEME_DIR/.config/gtk-3.0/ ;
+echo "done"
+#&&
+#./bootstrap-dell.fish &&
