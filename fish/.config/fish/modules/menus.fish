9ba6932a6774c4eedaaa3f674f3992987bdf82f7
actionless <actionless.loveless@gmail.com>
2015-01-10 14:47:45 +0100

    fix(fish: menus): remove unused argument

--- fish/.config/fish/modules/menus.fish
+++ fish/.config/fish/modules/menus.fish
@@ -1,13 +1,13 @@
 function tmux-menu -d "menu: tmux: switch to session"
 	tmux list-sessions \
-	| bemenu -l 20 \
+	| bemenu \
 	| sed -r 's/^([0-9]+):.*/\1/g' \
 	| xargs -n 1 -I \{\} tmux switch -t \{\}
 end
 
 function tmux-menu-switch -d "menu: tmux: switch to session (and close current)"
 	tmux list-sessions \
-	| bemenu -l 20 \
+	| bemenu \
 	| sed -r 's/^([0-9]+):.*/\1/g' \
 	| xargs -n 1 -I \{\} tmux switch -t \{\} \; kill-session -t (tmux display-message -p "#S")
 end
