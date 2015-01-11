ea020b8d83dcd068d06d8a085a27dcefd91fb067
actionless <actionless.loveless@gmail.com>
2015-01-11 02:52:59 +0100

    dmenu: init

---
+++ dmenu/.config/dmenu/dmenu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+FN='Monospace-8:Bold'
+
+# light:
+#NB='#d8d8d8'
+#NF='#101010'
+#SB='#ad7fa8'
+#SF='#000000'
+
+# lcars:
+NB='#cc6699'
+NF='#000000'
+SB='#000000'
+SF='#cc6699'
+
+OPTIONS='-b'
+eval "exec dmenu -fn '"$FN"' -nb '"$NB"' -nf '"$NF"' -sb '"$SB"' -sf '"$SF"' "$OPTIONS
