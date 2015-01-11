f16b6fd3b43a8f190bfa6605e4ee8f5c15a68c32
actionless <actionless.loveless@gmail.com>
2015-01-11 03:05:37 +0100

    chore(scripts: change-theme): added help prompt

--- change-theme.sh
+++ change-theme.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 stow -D $(./current-theme.sh) ;
 if test -z "$1"; then
-       	ls | grep theme- | bemenu | xargs stow
+	ls | grep theme- | bemenu -p "select theme:" | xargs stow
 else
 	stow "$1"
 fi
