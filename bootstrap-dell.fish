f634340ac9c10a606581f85570579c3665b67c58
actionless <actionless.loveless@gmail.com>
2015-01-11 02:52:37 +0100

    chore(scripts: bootstrap-dell): prettified output

--- bootstrap-dell.fish
+++ bootstrap-dell.fish
@@ -1,6 +1,12 @@
 #!/bin/fish
-for i in (ls | grep -v -e tp -e \. -e theme);
-	stow $i; or echo "failed: $i"
+
+for i in (ls | grep -v -e tp -e theme -e "\.");
+	echo -n "$i: " ;
+	if stow $i
+		echo "ok"
+	else
+		echo "FAILED"
+	end
 end
 
 ./change-theme.sh $argv
