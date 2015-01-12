aa39fd4adf7bf1f253bb789dee044f2b347b36bc
actionless <actionless.loveless@gmail.com>
2015-01-12 13:33:11 +0100

    fix(scripts: fix-lx): removed separation between dell and tp

--- fix-lxappearance.sh
+++ fix-lxappearance.sh
@@ -4,9 +4,15 @@ SCRIPT_DIR=$(dirname $(readlink -e "$0"))
 THEME_NAME=$($SCRIPT_DIR/current-theme.sh)
 THEME_DIR=$SCRIPT_DIR/$THEME_NAME
 
+PC_NAME="$1"
+test -z $PC_NAME &&
+echo "Usage: $0 PC_NAME" &&
+echo "Usage: $0 (dell|thinkpad)" &&
+exit 1
+
 mv ~/.gtkrc-2.0 $THEME_DIR/.gtkrc-2.0 ;
 mv ~/.config/gtk-3.0/settings.ini $THEME_DIR/.config/gtk-3.0/ ;
 
-./bootstrap-dell.sh $THEME_NAME &&
+./bootstrap-"$PC_NAME".sh $THEME_NAME &&
 
 echo "done"
