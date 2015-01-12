fdc3150295cfe95a3ed91f9c173d3ebfc37a3779
actionless <actionless.loveless@gmail.com>
2015-01-12 13:28:25 +0100

    fix(scripts: fix-lx): separate it for dell and tp

--- bootstrap-tp.fish
+++
@@ -1,3 +0,0 @@
-for i in (ls | grep -v -e dell -e \.md -e \.fish -e \.sh);
-	stow $i; or echo "failed: $i"
-end
---
+++ fix-lxappearance-thinkpad.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(dirname $(readlink -e "$0"))
+THEME_NAME=$($SCRIPT_DIR/current-theme.sh)
+THEME_DIR=$SCRIPT_DIR/$THEME_NAME
+
+mv ~/.gtkrc-2.0 $THEME_DIR/.gtkrc-2.0 ;
+mv ~/.config/gtk-3.0/settings.ini $THEME_DIR/.config/gtk-3.0/ ;
+
+./bootstrap-thinkpad.sh $THEME_NAME &&
+
+echo "done"
