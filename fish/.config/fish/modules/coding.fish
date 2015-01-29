662b1be6bfbb5933b06a10cd3742348ef83c8a45
actionless <actionless.loveless@gmail.com>
2015-01-29 19:06:48 +0100

    fix(fish: menus: tmux): don't close self session when switching to it

--- fish/.config/fish/modules/coding.fish
+++ fish/.config/fish/modules/coding.fish
@@ -37,7 +37,6 @@ end
 ###
 
 alias 'gcm' 'git commit -m'
-#make_completion git_status_pwd "git status"
 
 function git_status_pwd
 	command git status $argv | grep -v "\.\."
--- fish/.config/fish/modules/menus.fish
+++ fish/.config/fish/modules/menus.fish
@@ -1,15 +1,19 @@
 set -x BEMENU_BACKEND curses
 
-function tmux-menu -d "menu: tmux: switch to session"
+function tmux-switch -d "menu: tmux: switch to session"
 	tmux list-sessions \
 	| bemenu \
 	| sed -r 's/^([0-9]+):.*/\1/g' \
 	| xargs -n 1 -I \{\} tmux switch -t \{\}
 end
 
-function tmux-menu-switch -d "menu: tmux: switch to session (and close current)"
+function tmux-replace -d "menu: tmux: switch to session (and close current)"
+	set current_session (tmux display-message -p "#S")
 	tmux list-sessions \
 	| bemenu \
 	| sed -r 's/^([0-9]+):.*/\1/g' \
-	| xargs -n 1 -I \{\} tmux switch -t \{\} \; kill-session -t (tmux display-message -p "#S")
+	| while read i; set target_session "$target_session""$i"; end
+	if test ! -z $target_session -a  ! $target_session = $current_session
+		tmux switch -t $target_session \; kill-session -t $current_session
+	end
 end
