8c53521f18fa9acf5e2021017207e64f04ac8c86
actionless <actionless.loveless@gmail.com>
2015-04-03 02:37:53 +0200

    feat(nix: hw): add tmpfs

--- dell/nixos/hardware-configuration.nix
+++ dell/nixos/hardware-configuration.nix
@@ -13,14 +13,19 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4b1b780a-d474-4d64-b536-08c15de2891c";
-      fsType = "ext4";
-    };
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/713e9bd1-6c7a-46c2-b082-85e035c9b535";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/4b1b780a-d474-4d64-b536-08c15de2891c";
+    fsType = "ext4";
+  };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/713e9bd1-6c7a-46c2-b082-85e035c9b535";
+    fsType = "ext4";
+  };
+  fileSystems."/tmp" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = "size=512m";
+  };
 
   swapDevices = [
     { device = "/dev/disk/by-uuid/5737c44a-6bf1-4c7d-95bc-1ffe9082e971";
