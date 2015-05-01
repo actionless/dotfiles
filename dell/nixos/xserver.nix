739bcb762a9b98569000a2d64600da52a1c54c08
actionless <actionless.loveless@gmail.com>
2015-05-01 10:21:22 +0200

    fix(nix: x): use nvidia340

--- dell/nixos/xserver.nix
+++ dell/nixos/xserver.nix
@@ -13,33 +13,21 @@
       enable = true;
       layout = "us";
       xkbOptions = "eurosign:e";
-      #videoDrivers = [ "nvidia" ];
-      #deviceSection = ''
-        #Option "metamodes" "nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
-      #'';
-      videoDrivers = [ "nouveau" ];
+
+      videoDrivers = [ "nvidiaLegacy340" ];
       deviceSection = ''
-        Option "GLXVBlank" "true"
+       Option "metamodes" "nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
       '';
+
+      /*videoDrivers = [ "nouveau" ];*/
+      /*deviceSection = ''*/
+        /*Option "GLXVBlank" "true"*/
+      /*'';*/
+
       vaapiDrivers = [ pkgs.vaapiVdpau ];
       desktopManager.xterm.enable = false;
       windowManager.awesome.enable = false;
       displayManager.session = [ {
-        name = "awesome";
-        manage = "window";
-        start = ''
-          ${lib.concatMapStrings
-            (pkg: ''
-              export LUA_CPATH=$LUA_CPATH''${LUA_CPATH:+;}${pkg}/lib/lua/${pkgs.awesome.lua.luaversion}/?.so
-              export LUA_PATH=$LUA_PATH''${LUA_PATH:+;}${pkg}/lib/lua/${pkgs.awesome.lua.luaversion}/?.lua
-            '')
-            config.services.xserver.windowManager.awesome.luaModules
-          }
-          ${pkgs.awesome}/bin/awesome >> ~/.cache/awesome_stdout 2>> ~/.cache/awesome_stderr &
-          ${pkgs.coreutils}/bin/sleep 5 && ${pkgs.compton}/bin/compton &
-          waitPID=$!
-        '';
-      } {
         name = "awesome_no_ARGB";
         manage = "window";
         start = ''
@@ -50,7 +38,24 @@
             '')
             config.services.xserver.windowManager.awesome.luaModules
           }
+          ${pkgs.bash}/bin/bash /home/lie/.screenlayout/big_dell.sh
           ${pkgs.awesome}/bin/awesome --no-argb >> ~/.cache/awesome_stdout 2>> ~/.cache/awesome_stderr &
+          waitPID=$!
+        '';
+      } {
+        name = "awesome";
+        manage = "window";
+        start = ''
+          ${lib.concatMapStrings
+            (pkg: ''
+              export LUA_CPATH=$LUA_CPATH''${LUA_CPATH:+;}${pkg}/lib/lua/${pkgs.awesome.lua.luaversion}/?.so
+              export LUA_PATH=$LUA_PATH''${LUA_PATH:+;}${pkg}/lib/lua/${pkgs.awesome.lua.luaversion}/?.lua
+            '')
+            config.services.xserver.windowManager.awesome.luaModules
+          }
+          ${pkgs.bash}/bin/bash /home/lie/.screenlayout/big_dell.sh
+          ${pkgs.awesome}/bin/awesome >> ~/.cache/awesome_stdout 2>> ~/.cache/awesome_stderr &
+          ${pkgs.coreutils}/bin/sleep 5 && ${pkgs.compton}/bin/compton &
           waitPID=$!
         '';
       } ];
