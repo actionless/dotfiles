0c225976e3d0d25a7013597e93fb892f3a820579
actionless <actionless.loveless@gmail.com>
2015-05-14 16:01:03 +0200

    fix(fish: menus): tmux session names

--- fish/.config/fish/modules/menus.fish
+++ fish/.config/fish/modules/menus.fish
@@ -3,7 +3,7 @@ set -x BEMENU_BACKEND curses
 function tmux-switch -d "menu: tmux: switch to session"
 	tmux list-sessions \
 	| bemenu \
-	| sed -r 's/^([0-9]+):.*/\1/g' \
+	| sed -r 's/^(\w+):.*/\1/g' \
 	| xargs -n 1 tmux switch -t
 end
 
@@ -11,7 +11,7 @@ function tmux-replace -d "menu: tmux: switch to session (and close current)"
 	set current_session (tmux display-message -p "#S")
 	tmux list-sessions \
 	| bemenu \
-	| sed -r 's/^([0-9]+):.*/\1/g' \
+	| sed -r 's/^(\w+):.*/\1/g' \
 	| while read i; set target_session "$target_session""$i"; end
 	if test ! -z $target_session -a  ! $target_session = $current_session
 		tmux switch -t $target_session \; kill-session -t $current_session
