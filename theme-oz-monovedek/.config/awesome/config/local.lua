3c26a6053fd1494997d68f765b262d87c9fa7a1d
actionless <actionless.loveless@gmail.com>
2015-07-15 11:29:36 +0200

    chore: add configs for temporary workstation

---
+++ theme-oz-monovedek/.config/awesome/config/local.lua
@@ -0,0 +1,36 @@
+local awful = require("awful")
+
+
+local local_config = {}
+
+function local_config.init(awesome_context)
+
+  local function set_colorscheme(part_path)
+    awesome_context.theme_dir = awful.util.getdir("config") ..
+      "/themes/" .. part_path .. "/theme.lua"
+  end
+
+  awesome_context.config = {
+    wlan_if = 'wlp12s0',
+    eth_if = 'enp0s25',
+    net_preset = 'systemd',
+    cpu_cores_num =2,
+    music_players = { 'spotify', 'clementine' },
+  }
+  awesome_context.volume_widget = "apw"
+  --set_colorscheme("lcars")
+  --set_colorscheme("lcars_modern")
+  --set_colorscheme("pokemon_dark")
+  --set_colorscheme("pokemon_light")
+  --set_colorscheme("monovedek")
+  --set_colorscheme("pro-medium-light")
+  --set_colorscheme("vertex")
+  --set_colorscheme("transparent-xresources")
+  --set_colorscheme("lcars-xresources")
+  --set_colorscheme("noble_dark")
+  --set_colorscheme("pokemon-xresources")
+  set_colorscheme("lcars-xresources-hidpi")
+  --set_colorscheme("pro-medium-light")
+end
+
+return local_config
