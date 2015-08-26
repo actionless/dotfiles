164421a8c3dfb764324f1c3c1bc98fc0ce4705ee
actionless <actionless.loveless@gmail.com>
2015-08-27 00:15:56 +0200

    fix(fish: menus): return back 'tmr'

--- fish/.config/fish/modules/menus.fish
+++ fish/.config/fish/modules/menus.fish
@@ -1,8 +1,13 @@
 set -x BEMENU_BACKEND curses
 
-function tmux-switch -d "menu: tmux: switch to session"
+function tmux-replace -d "menu: tmux: switch to session (and close current)"
+	set current_session (tmux display-message -p "#S")
 	tmux list-sessions \
 	| bemenu \
 	| sed -r 's/^(\w+):.*/\1/g' \
-	| xargs -n 1 tmux switch -t
+	| while read i; set target_session "$target_session""$i"; end
+	if test ! -z $target_session -a  ! $target_session = $current_session
+		tmux switch -t $target_session \; kill-session -t $current_session
+	end
 end
+alias tmr tmux-replace
