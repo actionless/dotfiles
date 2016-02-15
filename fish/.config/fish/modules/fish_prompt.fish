f4e2eae9166eb8060f97b1d7a3491c6780655afe
actionless <actionless.loveless@gmail.com>
2016-02-15 18:22:50 +0100

    feat(fish: prompt): generalize decorations

--- fish/.config/fish/modules/fish_prompt.fish
+++ fish/.config/fish/modules/fish_prompt.fish
@@ -20,6 +20,9 @@ function git_prompt
 	if [ $git_branch_name ]
 		if [ (_is_git_dirty) ]
 			set_color red -o
+			set git_branch_name_uncommited (git status --porcelain 2>/dev/null| grep "^ [MD]" | wc -l)
+			set git_branch_name_unstaged (git status --porcelain 2>/dev/null| grep "^ \\\?\\\?" | wc -l)
+			set git_branch_addition " ($git_branch_name_uncommited/$git_branch_name_unstaged)"
 		else
 			if [ (_get_git_ahead_count $git_branch_name) -gt 0 ]
 				set_color green -o
@@ -28,7 +31,7 @@ function git_prompt
 	else
 		return 1
 	end
-	echo " ""$git_branch_name"" "
+	echo " ""$git_branch_name""$git_branch_addition"" "
 end
 
 function fish_prompt --description 'Write out the prompt'
@@ -52,18 +55,28 @@ function fish_prompt --description 'Write out the prompt'
 	set -l inverse "\e[7m"
 
 	if [ $last_status -ne 0 ]
-		set prompt_status (set_color yellow -o) $inverse " ""$last_status"" " (set_color -b normal) " "
+		set prompt_status (set_color yellow -o) $inverse " ""$last_status"" " (set_color -b normal) $fish_prompt_mid_separator
 	end
 
+	#set -x fish_prompt_left_separator ""
+	#set -x fish_prompt_right_separator ""
+	#set -x fish_prompt_mid_separator " "
+
+	set -x fish_prompt_left_separator "퟼"
+	set -x fish_prompt_right_separator "퟽"
+	set -x fish_prompt_mid_separator ""
+
 	echo -e -n -s \
-(set_color $fish_color_cwd) "\n " (set_color -b normal) " " \
+"\n " \
+(set_color $fish_color_cwd) $fish_prompt_left_separator \
+(set_color -b normal) $fish_prompt_mid_separator \
 $inverse (set_color $user_color) " "$USER " " $__fish_prompt_hostname" " \
-(set_color -b normal) " " \
+(set_color -b normal) $fish_prompt_mid_separator \
 $inverse (set_color $fish_color_cwd) " "(pwd)" " \
-(set_color -b normal) " " \
-$inverse (set_color $fish_color_cwd) (git_prompt ; and echo (set_color -b normal)" ") \
+(set_color -b normal) $fish_prompt_mid_separator \
+$inverse (set_color $fish_color_cwd -o) (git_prompt ; and echo (set_color -b normal)$fish_prompt_mid_separator) \
 $prompt_status \
-(set_color -b normal $fish_color_cwd) " " \
+(set_color -b normal $fish_color_cwd) $fish_prompt_right_separator " " \
 (set_color -b normal) \
 $__fish_prompt_normal "\n" \
 $user_prompt " "
