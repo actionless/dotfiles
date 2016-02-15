ca4312230dd76fbbc65912c3d9dd63012e418e28
actionless <actionless.loveless@gmail.com>
2016-02-15 18:26:12 +0100

    feat(fish: init): save config cache to the separate user file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
 #!/usr/sbin/fish
 
-if [ ! -e /tmp/config.fish ]
+if [ ! -e /tmp/config-"$USER".fish ]
 	echo "Writing config to tmpfs..."
-	cat ~/.config/fish/modules/*.fish > /tmp/config.fish
+	cat ~/.config/fish/modules/*.fish > /tmp/config-"$USER".fish
 end
 
-source /tmp/config.fish
+source /tmp/config-"$USER".fish
