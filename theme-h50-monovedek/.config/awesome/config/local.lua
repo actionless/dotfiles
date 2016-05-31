7e9d61067996ee12d23772f72aa8c4d9c721f5b6
actionless <actionless.loveless@gmail.com>
2016-06-01 00:25:35 +0200

    chore(theme-h50-monovedek: awesome): sync

--- theme-h50-monovedek/.config/awesome/config/local.lua
+++ theme-h50-monovedek/.config/awesome/config/local.lua
@@ -4,31 +4,24 @@ local awful = require("awful")
 local local_config = {}
 
 function local_config.init(awesome_context)
-  awesome_context.config = {
-    wlan_if = 'wlp4s0',
-    eth_if = 'enp0s25',
-    net_preset = 'systemd',
-    music_players = { 'spotify', 'clementine', },
-  }
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/vertex/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars_xresources/theme.lua"
+
+  local conf = awesome_context.config
+  conf.wlan_if = 'wlp4s0'
+  conf.eth_if = 'enp0s25'
+  conf.net_preset = 'systemd'
+  conf.music_players = { 'spotify', 'clementine', 'mopidy' }
+  --conf.music_players = { 'mopidy', }
+
   awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/pokemon_dark/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/monovedek/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/pokemon_light/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars_modern/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/noble_dark/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/pokemon-xresources/theme.lua"
-  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/pro-medium-light/theme.lua"
+
   awesome_context.autorun = {
     --"~/.scripts/tp_unmute",
     --"killall compton ; compton",
   }
-  awesome_context.volume_widget='apw'
+
   awesome_context.have_battery = false
   awesome_context.sensor = "temp1"
   awesome_context.new_top = true
-  awesome_context.lcarslist_enabled = false
 
   return awesome_context
 end
