8b1896d4febc4c836f02649f005bfbc377011cc9
actionless <actionless.loveless@gmail.com>
2017-02-11 16:09:24 +0100

    theme(h50-retrox): update oomox theme name

--- theme-h50-retrox/.config/awesome/config/local.lua
+++ theme-h50-retrox/.config/awesome/config/local.lua
@@ -15,7 +15,7 @@ function local_config.init(awesome_context)
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
   awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/twmish/theme.lua"
-  OOMOX_THEME_NAME = 'twm_reloaded'
+  OOMOX_THEME_NAME = 'retro/classic_x_new'
   --OOMOX_SEL_BORDER = "#b03060"
   --OOMOX_BORDER = "#ACb3AC"
 
