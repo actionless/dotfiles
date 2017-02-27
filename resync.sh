e71c89da0c6e47b48a61430b5c769a75265f38b9
actionless <actionless.loveless@gmail.com>
2017-02-27 13:59:10 +0100

    fix(resync): strip workstation name

--- resync.sh
+++ resync.sh
@@ -5,12 +5,13 @@ SCRIPT_DIR=$(dirname $(readlink -e "$0"))
 THEME_NAME=$($SCRIPT_DIR/current-theme.sh)
 THEME_DIR=$SCRIPT_DIR/$THEME_NAME
 
-PC_NAME="$1"
+PC_NAME="${1-}"
 if [ -z $PC_NAME ] ; then
 	echo "Usage: $0 PC_NAME"
 	echo "Usage: $0 (dell|thinkpad)"
 	exit 1
 fi
+PC_NAME=$(basename ${PC_NAME})
 
 test ! -L ~/.gtkrc-2.0 && mv ~/.gtkrc-2.0 $THEME_DIR/.gtkrc-2.0
 test ! -L ~/.config/gtk-3.0/settings.ini && mv ~/.config/gtk-3.0/settings.ini $THEME_DIR/.config/gtk-3.0/settings.ini
