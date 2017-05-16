fa125508ba061809e4e4c91a7f5fd72414b9c4bb
actionless <actionless.loveless@gmail.com>
2017-05-17 00:52:50 +0200

    feat(fish: config): export user's dbus env var by default

--- fish/.config/fish/modules/30-config.fish
+++ fish/.config/fish/modules/30-config.fish
@@ -8,6 +8,7 @@ set -x LC_ALL "en_US.UTF-8"
 #set --universal fish_user_paths $fish_user_paths $HOME/.gem/ruby/2.1.0/bin/;
 #set --universal fish_user_paths $fish_user_paths $HOME/scripts/;
 eval (dircolors -c ~/.dir_colors | sed 's/>&\/dev\/null$//')
+set -x DBUS_SESSION_BUS_ADDRESS "unix:path=/run/user/"(id -u)"/bus"
 
 function fish_user_key_bindings
 	#bind \eOD backward-word
