c4bbf410519acd629e5060dad8c404171446056c
actionless <actionless.loveless@gmail.com>
2017-07-31 21:12:39 +0200

    feat(change-theme.sh): handle cancellation

--- change-theme.sh
+++ change-theme.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-stow -D $(./current-theme.sh) ;
-if test -z "$1"; then
-	ls | grep theme- | grep $(./current-workstation.sh | sed 's/workstation-//') | bemenu -p "select theme:" | xargs stow
+set -ueo pipefail
+
+if [ -z "${1:-}" ]; then
+	STOW_TARGET=$(ls | grep theme- | grep $(./current-workstation.sh | sed 's/workstation-//') | bemenu -p "select theme:") || (echo "Canceled" ; exit 1)
 else
-	stow "$1"
+	STOW_TARGET=$1
 fi
+
+stow -D $(./current-theme.sh) ;
+stow "$STOW_TARGET"
+
+set +ueo pipefail
+
 xrdb -merge $HOME/.Xresources
 source ~/.profile
 echo "awesome.restart()" | awesome-client || true
 pgrep "^st\$" | xargs kill -s USR1
-for line in $(env | grep -e ^TMUX_ -e ^FISH_ -e ^TERM_ | grep -v TMUX_PANE) ; do tmux setenv -g $(tr '=' ' ' <<< $line) ; done;
+for line in $(env | grep -e ^TMUX_ -e ^FISH_ -e ^TERM_ | grep -v TMUX_PANE) ; do
+	tmux setenv -g $(tr '=' ' ' <<< $line) ;
+done;
 tmux source-file ~/.tmux.conf
 fish -c reload_fish
