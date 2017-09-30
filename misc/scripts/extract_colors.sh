6b3b19e9ae1526bdbcad16dc41deb1dbc4328975
actionless <actionless.loveless@gmail.com>
2017-09-30 22:56:02 +0200

    refactor(misc: scripts: extract_colors): use functions

--- misc/scripts/extract_colors.sh
+++ misc/scripts/extract_colors.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-INPUT_PATH=${1:-assets.svg}
+PATHLIST=${@:-'assets.svg'}
+
 
 hextoi() {
 	bc <<< "ibase=16; ${1}"
 }
 
+parse_channel() {
+	hextoi $(cut -c${2} <<< ${1})
+}
 
-for hexinput in $(grep -Eo "#[0-9a-fA-F]{6}" ${INPUT_PATH} | tr '[:lower:]' '[:upper:]' | tr -d "#" | sort | uniq -c | sort -rn ) ; do
-	a=$(hextoi $(cut -c-2 <<< ${hexinput}))
-	b=$(hextoi $(cut -c3-4 <<< ${hexinput}))
-	c=$(hextoi $(cut -c5-6 <<< ${hexinput}))
-	if [[ -z ${c} ]] ; then
+print_rgba(){
+	text=$1
+	r=$2
+	g=$3
+	b=$4
+	printf "#\x1b[38;2;${r};${g};${b}m$text\x1b[0m\n"
+}
+
+for hexinput in $(
+	grep --extended-regexp --only-matching --no-filename "#[0-9a-fA-F]{6}" ${PATHLIST} \
+	| tr '[:lower:]' '[:upper:]' \
+	| tr -d "#" \
+	| sort | uniq -c | sort -rn
+) ; do
+	r=$(parse_channel ${hexinput}  -2)
+	g=$(parse_channel ${hexinput} 3-4)
+	b=$(parse_channel ${hexinput} 5-6)
+	if [[ -z ${b} ]] ; then
 		printf "%4s: " "${hexinput}"
 	else
-		printf "#\x1b[38;2;${a};${b};${c}m$hexinput\x1b[0m\n"
+		print_rgba ${hexinput} ${r} ${g} ${b}
 	fi
 done
