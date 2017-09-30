fe26215901123dab855eb4fb3bf7e72ebf3df61a
actionless <actionless.loveless@gmail.com>
2017-09-30 22:55:19 +0200

    chore: add theme-h50-unit01c

---
+++ theme-h50-unit01c/.config/awesome/config/local.lua
@@ -0,0 +1,31 @@
+local awful = require("awful")
+
+
+local local_config = {}
+
+function local_config.init(awesome_context)
+
+  local conf = awesome_context.config
+  conf.wlan_if = 'wlp4s0'
+  conf.eth_if = 'enp0s25'
+  conf.net_preset = 'systemd'
+  conf.music_players = { 'spotify', 'clementine', 'mopidy' }
+  --conf.music_players = { 'mopidy', }
+
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
+  awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
+  OOMOX_THEME_NAME = 'popart/unit_01d'
+
+  awesome_context.autorun = {
+    --"~/.scripts/tp_unmute",
+    --"killall compton ; compton",
+  }
+
+  awesome_context.have_battery = false
+  awesome_context.sensor = "temp1"
+  awesome_context.new_top = true
+
+  return awesome_context
+end
+
+return local_config
