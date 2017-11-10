411c08083a49fe89c471fa0795e42182832b7d0e
actionless <actionless.loveless@gmail.com>
2017-11-10 13:44:56 +0100

    chore(fish: lib): remove commented out code

--- fish/.config/fish/modules/10-lib.fish
+++ fish/.config/fish/modules/10-lib.fish
@@ -1,26 +1,5 @@
 set FISH (which fish)
 
-#{{{ command not found
-#function __fish_command_not_found_handler --on-event fish_command_not_found
-
-	## alternative implementation:
-	##cnf-lookup $argv
-
-	#set cmd $argv[1]
-	#set pkgs (pkgfile -b -v -- "$cmd" 2>/dev/null)
-	#if [ (count $pkgs) -ge 1 ]
-		#set_color green
-		#printf '%s may be found in the following packages:\n' "$cmd"
-		#set_color normal
-		#for pkg in $pkgs
-			#printf '	%s\n' "$pkg"
-		#end
-	#else
-		#printf "fish: %s: command not found :(\n" "$cmd" >&2
-	#end
-#end
-#}}}
-
 function _cdr -d "CDR, CAR's friend"
 	set -l new_argv
 	if test (count $argv) -gt 1;
