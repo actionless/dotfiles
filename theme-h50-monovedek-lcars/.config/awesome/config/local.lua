e47d97454c4d62f3550cd2f1ba0f16085e576483
actionless <actionless.loveless@gmail.com>
2017-11-21 19:46:32 +0100

    theme(h50: monovedek-lcars): switch to Fantasque Sans Mono font

--- theme-h50-monovedek-lcars/.config/awesome/config/local.lua
+++ theme-h50-monovedek-lcars/.config/awesome/config/local.lua
@@ -12,7 +12,8 @@ function local_config.init(awesome_context)
   conf.music_players = { 'spotify', 'clementine', 'mopidy' }
   --conf.music_players = { 'mopidy', }
 
-  awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
+  awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
 
   awesome_context.autorun = {
     --"~/.scripts/tp_unmute",
@@ -22,6 +23,38 @@ function local_config.init(awesome_context)
   awesome_context.have_battery = false
   awesome_context.sensor = "temp1"
   awesome_context.new_top = true
+
+  awesome_context.before_config_loaded = function()
+    local beautiful = require("beautiful")
+    local dpi = beautiful.xresources.apply_dpi
+    -- size fixes for Fanstasque Sans Mono:
+    --local beautiful = require("beautiful")
+    beautiful.font = "Monospace Bold 11"
+    beautiful.tasklist_font = "Monospace 11"
+    beautiful.panel_widget_font = beautiful.tasklist_font
+    beautiful.taglist_font =  beautiful.font
+    beautiful.titlebar_font =  beautiful.font
+    beautiful.small_font = "Monospace 8"
+    beautiful.sans_font = "Sans 11"
+    beautiful.notification_font = beautiful.sans_font
+    beautiful.text_font = beautiful.sans_font
+    -- Height fixes for that font:
+    beautiful.border_radius = dpi(8)
+    beautiful.panel_widget_border_radius = dpi(7)
+    beautiful.border_focus = beautiful.xrdb.color10
+    beautiful.basic_panel_height = dpi(20)
+    beautiful.panel_padding_bottom = dpi(3)
+    beautiful.panel_height = beautiful.basic_panel_height + beautiful.panel_padding_bottom
+    beautiful.titlebar_height = beautiful.basic_panel_height + beautiful.base_border_width*2
+
+    -- Use music style from xresources theme:
+    beautiful.widget_music_bg = beautiful.xrdb.color11
+    beautiful.apw_fg_color = beautiful.xrdb.color14
+    beautiful.apw_bg_color = beautiful.panel_widget_bg
+  end
+
+  --awesome_context.after_config_loaded = function()
+  --end
 
   return awesome_context
 end
