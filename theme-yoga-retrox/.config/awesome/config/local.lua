19982cacf4c16f4be27a894620e0d8d068e64c83
actionless <actionless.loveless@gmail.com>
2017-12-06 02:57:18 +0100

    chore(themes: *: retrox: awesome): remove old unused global

--- theme-yoga-retrox/.config/awesome/config/local.lua
+++ theme-yoga-retrox/.config/awesome/config/local.lua
@@ -15,9 +15,6 @@ function local_config.init(awesome_context)
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
   awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/twmish/theme.lua"
-  OOMOX_THEME_NAME = 'retro/classic_x_new'
-  --OOMOX_SEL_BORDER = "#b03060"
-  --OOMOX_BORDER = "#ACb3AC"
 
   awesome_context.autorun = {
     --"~/.scripts/tp_unmute",
