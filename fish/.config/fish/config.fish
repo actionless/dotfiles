0b5ea5489797d653621784f818084224149fed04
actionless <actionless.loveless@gmail.com>
2017-12-12 15:45:42 +0100

    feat(fish): update prompt on all fish instances when TERM_DECORATION_* env vars are set again by theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,9 @@
 if [ ! -e /tmp/config-"$USER".fish ]
 	echo "Writing config to tmpfs..."
 	cat ~/.config/fish/modules/*.fish > /tmp/config-"$USER".fish
+	set -U -x fish_prompt_left_separator $TERM_DECORATION_LEFT
+	set -U -x fish_prompt_right_separator $TERM_DECORATION_RIGHT
+	set -U -x fish_prompt_mid_separator $TERM_DECORATION_SEPARATOR
 end
 
 source /tmp/config-"$USER".fish
