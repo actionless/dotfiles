dfd5b1319768384fc142b098963bc90994684712
actionless <actionless.loveless@gmail.com>
2017-12-12 15:46:07 +0100

    style(change-theme): insert some newlines to make it more readable

--- change-theme.sh
+++ change-theme.sh
@@ -2,7 +2,12 @@
 set -ueo pipefail
 
 if [ -z "${1:-}" ]; then
-	STOW_TARGET=$(ls | grep theme- | grep $(./current-workstation.sh | sed 's/workstation-//') | fzf --prompt="select theme:") || (echo "Canceled" ; exit 1)
+	STOW_TARGET=$(\
+		ls | grep theme- | grep $(./current-workstation.sh | sed 's/workstation-//') | fzf --prompt="select theme:"\
+	) || (
+		echo "Canceled"
+		exit 1
+	)
 else
 	STOW_TARGET=$1
 fi
@@ -15,8 +20,8 @@ stow "$STOW_TARGET"
 
 set +ueo pipefail
 
-xrdb -merge $HOME/.Xresources
 source ~/.profile
+xrdb -merge $HOME/.Xresources
 echo "awesome.restart()" | awesome-client || true
 pgrep "^st\$" | xargs kill -s USR1
 for line in $(env | grep -e ^TMUX_ -e ^FISH_ -e ^TERM_ | grep -v TMUX_PANE) ; do
