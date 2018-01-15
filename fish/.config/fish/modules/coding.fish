293ae542fbc78a70656d6213f49e0bd89dd09edc
actionless <actionless.loveless@gmail.com>
2018-01-15 05:54:29 +0100

    feat(fish: coding): add `rgr` function to quickly replace using `rg` and `sed`

--- fish/.config/fish/modules/coding.fish
+++ fish/.config/fish/modules/coding.fish
@@ -4,14 +4,21 @@
 
 alias sprunge "curl -F 'sprunge=<-' http://sprunge.us"
 
-alias a_grep "rg --hidden"
-
+alias a_grep "rg"
 function a_replace -a grep_line original_value replace_value -d "replace stuff using ag and sed"
 	if test (count $argv) -eq 2;
 		set replace_value $original_value
 		set original_value $grep_line
 	end
 	a_grep -l --null "$grep_line" \
+	| xargs -0 -n 1 sed -i -e 's/'"$original_value"'/'"$replace_value"'/g'
+end
+function rgr -a original_value replace_value -d "replace stuff using ag and sed"
+	set -l rest_argv
+	if test (count $argv) -gt 2;
+		set rest_argv (_cdr (_cdr $argv))
+	end
+	rg $rest_argv -l --null "$original_value" \
 	| xargs -0 -n 1 sed -i -e 's/'"$original_value"'/'"$replace_value"'/g'
 end
 
