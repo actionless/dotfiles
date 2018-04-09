c7a1e0ecc490d14cc14c334a26181d9ec8393f41
actionless <actionless.loveless@gmail.com>
2018-04-09 21:36:35 +0200

    fix(resync.sh): ignore missing gtk2 or gtk3 legacy configs

--- resync.sh
+++ resync.sh
@@ -16,8 +16,8 @@ if [ -z $PC_NAME ] ; then
 fi
 PC_NAME=$(basename ${PC_NAME})
 
-test ! -L ~/.gtkrc-2.0 && mv ~/.gtkrc-2.0 $THEME_DIR/.gtkrc-2.0
-test ! -L ~/.config/gtk-3.0/settings.ini && mv ~/.config/gtk-3.0/settings.ini $THEME_DIR/.config/gtk-3.0/settings.ini
+test ! -L ~/.gtkrc-2.0 && mv ~/.gtkrc-2.0 $THEME_DIR/.gtkrc-2.0 || true
+test ! -L ~/.config/gtk-3.0/settings.ini && mv ~/.config/gtk-3.0/settings.ini $THEME_DIR/.config/gtk-3.0/settings.ini || true
 
 ./bootstrap.sh "$PC_NAME" $THEME_NAME
 
