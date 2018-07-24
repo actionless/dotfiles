e968cb27d25109d9d9e508757f3ee7f1026d6510
actionless <actionless.loveless@gmail.com>
2018-07-24 19:35:37 +0200

    refactor(fish: menus): rewrite to use `fzf` or fallback to `dialog` if fzf is not present

--- fish/.config/fish/modules/menus.fish
+++ fish/.config/fish/modules/menus.fish
@@ -1,37 +1,28 @@
+function dialog_menu
+	set -l choices_file (mktemp)
+	cat | sed -E 's/(.*)/\1\n/g' > $choices_file
+	set -l dialog_args (cat $choices_file)
+	rm $choices_file
 
-function tmux-replace-b -d "menu: tmux: switch to session (and close current)"
+	set -l result (dialog --menu test_dialog_title 0 0 0 $dialog_args --output-fd 1)
+	echo $result
+end
+
+function my_fish_menu
+	if which fzf
+		fzf --height=10 $argv
+	else
+		dialog_menu $argv
+	end
+end
+
+function tmr -d "menu: tmux: switch to session (and close current)"
 	set current_session (tmux display-message -p "#S")
 	tmux list-sessions \
-	| env BEMENU_BACKEND=curses bemenu \
+	| my_fish_menu \
 	| sed -r 's/^(\w+):.*/\1/g' \
 	| while read i; set target_session "$target_session""$i"; end
 	if test ! -z $target_session -a  ! $target_session = $current_session
 		tmux switch -t $target_session \; kill-session -t $current_session
 	end
 end
-alias tmrb tmux-replace-b
-
-function tmux-replace-p -d "menu: tmux: switch to session (and close current)"
-	set current_session (tmux display-message -p "#S")
-	tmux list-sessions \
-	| pmenu \
-	| sed -r 's/^(\w+):.*/\1/g' \
-	| while read i; set target_session "$target_session""$i"; end
-	if test ! -z $target_session -a  ! $target_session = $current_session
-		tmux switch -t $target_session \; kill-session -t $current_session
-	end
-end
-alias tmrp tmux-replace-p
-
-
-function tmux-replace-f -d "menu: tmux: switch to session (and close current)"
-	set current_session (tmux display-message -p "#S")
-	tmux list-sessions \
-	| fzf --height=10 \
-	| sed -r 's/^(\w+):.*/\1/g' \
-	| while read i; set target_session "$target_session""$i"; end
-	if test ! -z $target_session -a  ! $target_session = $current_session
-		tmux switch -t $target_session \; kill-session -t $current_session
-	end
-end
-alias tmr tmux-replace-f
