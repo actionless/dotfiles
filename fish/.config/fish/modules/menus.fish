19ce3d8802cd2508759d75973e32337a43d02a6c
actionless <actionless.loveless@gmail.com>
2018-07-28 17:43:39 +0200

    fix(fish: menus): print error messsage if neither `dialog` nor `fzf` are installed

--- fish/.config/fish/modules/menus.fish
+++ fish/.config/fish/modules/menus.fish
@@ -9,20 +9,24 @@ function dialog_menu
 end
 
 function my_fish_menu
-	if which fzf
+	if which fzf > /dev/null
 		fzf --height=10 $argv
-	else
+	else if which dialog > /dev/null
 		dialog_menu $argv
+	else
+		echo
+		echo "Either `fzf` or `dialog` should be installed."
+		return 1
 	end
 end
 
 function tmr -d "menu: tmux: switch to session (and close current)"
 	set current_session (tmux display-message -p "#S")
 	tmux list-sessions \
-	| my_fish_menu \
+	| begin my_fish_menu ; or return ; end \
 	| sed -r 's/^(\w+):.*/\1/g' \
 	| while read i; set target_session "$target_session""$i"; end
-	if test ! -z $target_session -a  ! $target_session = $current_session
+	if test ! -z "$target_session" -a  ! "$target_session" = $current_session
 		tmux switch -t $target_session \; kill-session -t $current_session
 	end
 end
