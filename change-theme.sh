9733726d4a6bbf7114c3031c8b9c470e58a0fe6d
actionless <actionless.loveless@gmail.com>
2018-10-03 07:29:28 +0200

    feat(change-theme): more verbose script output

--- change-theme.sh
+++ change-theme.sh
@@ -15,19 +15,27 @@ fi
 
 CURRENT_THEME=$(./current-theme.sh) || CURRENT_THEME=''
 if [[ ! -z ${CURRENT_THEME} ]] ; then
+	echo "== Unstowing the current theme..."
 	stow -D "$(./current-theme.sh)"
 fi
+echo "== Stowing the new theme..."
 stow "$STOW_TARGET"
 
 set +ueo pipefail
 
+echo "== Applying the new theme..."
 # shellcheck source=$HOME/.profile disable=SC1091
+echo "-- Applying profile..."
 source ~/.profile
+echo "-- Applying xrdb..."
 xrdb -merge "$HOME"/.Xresources
 if [[ -z "${NORESTART:-}" ]] ; then
+	echo "-- Restarting AwesomeWM..."
 	echo "awesome.restart()" | awesome-client || true
 fi
+echo "-- Reloading xst..."
 pgrep "^xst\$" | xargs kill -s USR1
+echo "-- Applying env inside tmux sessions..."
 for line in $(env | grep -e ^TMUX_ -e ^FISH_ -e ^TERM_ | grep -v TMUX_PANE) ; do
 	# shellcheck disable=SC2046
 	key=$(cut -d'=' -f1 <<< "$line")
@@ -38,10 +46,13 @@ for line in $(env | grep -e ^TMUX_ -e ^FISH_ -e ^TERM_ | grep -v TMUX_PANE) ; do
 		tmux setenv -g "${key}" "${value:-}"
 	fi
 done;
+echo "-- Reloading tmux..."
 tmux source-file ~/.tmux.conf
+echo "-- Reloading fish..."
 fish -c reload_fish
 
 if [[ -f ~/.apply_theme.sh ]] ; then
+	echo "-- Executing '~/.apply_theme.sh' script..."
 	~/.apply_theme.sh
 fi
 
