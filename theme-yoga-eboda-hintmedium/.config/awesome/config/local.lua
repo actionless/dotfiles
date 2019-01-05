384054e313ec5d195001a1bdec45df2688e178f1
actionless <actionless.loveless@gmail.com>
2019-01-05 06:12:40 +0100

    chore(themes: yoga: eboda): split medium font hinting as a separate theme

---
+++ theme-yoga-eboda-hintmedium/.config/awesome/config/local.lua
@@ -0,0 +1,64 @@
+local awful = require("awful")
+
+
+local local_config = {}
+
+function local_config.init(awesome_context)
+
+  local conf = awesome_context.config
+  conf.wlan_if = 'wlp4s0'
+  conf.eth_if = 'enp0s25'
+  conf.net_preset = 'systemd'
+  conf.music_players = { 'spotify', 'clementine', 'mopidy' }
+  --conf.music_players = { 'mopidy', }
+
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
+  awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/twmish/theme.lua"
+
+  awesome_context.autorun = {
+    --"~/.scripts/tp_unmute",
+    --"killall compton ; compton",
+  }
+
+  awesome_context.have_battery = true
+  awesome_context.sensor = "temp1"
+  awesome_context.new_top = true
+
+  awesome_context.before_config_loaded = function()
+    local beautiful = require('beautiful')
+    local dpi = beautiful.xresources.apply_dpi
+    local color_utils = require("actionless.util.color")
+    local string_utils = require("actionless.util.string")
+    local gsc = beautiful.get().gtk
+    local xrdb = beautiful.get().xrdb
+
+    -- force pseudo-shadow client decorations:
+    --beautiful.border_shadow_width = dpi(4)
+    --beautiful.titlebar_shadow_focus = string_utils.max_length(gsc.fg_color, 7).."cc"
+    --beautiful.titlebar_shadow_normal = string_utils.max_length(gsc.fg_color, 7).."38"
+    --beautiful.titlebar_bg_normal = "#00000000"
+    --beautiful.titlebar_bg_focus = "#00000000"
+
+    --local ACCENT_COLOR = "#dab1ff"
+    --beautiful.border_focus = color_utils.darker(ACCENT_COLOR, 47)
+    local ACCENT_COLOR = gsc.selected_bg_color
+    beautiful.border_focus = ACCENT_COLOR
+
+    -- custom color for music widget:
+    --beautiful.widget_music_bg = color_utils.mix(
+      --beautiful.panel_fg, gsc.selected_bg_color, 0.67
+    --)
+    beautiful.widget_music_bg = ACCENT_COLOR
+
+    -- set all fonts to bold
+    beautiful.font = beautiful.titlebar_font
+    beautiful.tasklist_font = beautiful.font
+  end
+
+  awesome_context.apw_on_the_left= true
+
+  return awesome_context
+end
+
+return local_config
