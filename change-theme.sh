69a160e9b648e84862c8f7250e3c0df8d90cf3f0
actionless <actionless.loveless@gmail.com>
2019-01-05 06:08:54 +0100

    wip(change-theme.sh): move xst reload to the end

--- change-theme.sh
+++ change-theme.sh
@@ -2,13 +2,14 @@
 set -ueo pipefail
 
 warn() {
+	# shellcheck disable=SC2145
 	echo "[33m$@ [30m[m"
 }
 
 if [ -z "${1:-}" ]; then
 	# shellcheck disable=SC2010
 	STOW_TARGET=$(\
-		ls | grep theme- | grep "$(./current-workstation.sh | sed 's/workstation-//')" | fzf --prompt="select theme:"\
+		ls | grep theme- | grep "$(./current-workstation.sh | sed 's/workstation-//')" | fzf --prompt="select theme:" \
 	) || (
 		warn "Canceled"
 		exit 1
@@ -18,27 +19,30 @@ else
 fi
 
 CURRENT_THEME=$(./current-theme.sh) || CURRENT_THEME=''
-if [[ ! -z ${CURRENT_THEME} ]] ; then
+
+if [[ -n ${CURRENT_THEME} ]] ; then
 	warn "== Unstowing the current theme..."
 	stow -D "$(./current-theme.sh)"
 fi
+
 warn "== Stowing the new theme..."
 stow "$STOW_TARGET"
 
 set +ueo pipefail
 
 warn "== Applying the new theme..."
-# shellcheck source=$HOME/.profile disable=SC1091
+
 warn "-- Applying profile..."
+# shellcheck source=$HOME/.profile disable=SC1091
 source ~/.profile
+
 warn "-- Applying xrdb..."
 xrdb -merge "$HOME"/.Xresources
 if [[ -z "${NORESTART:-}" ]] ; then
 	warn "-- Restarting AwesomeWM..."
 	echo "awesome.restart()" | awesome-client || true
 fi
-warn "-- Reloading xst..."
-pgrep "^xst\$" | xargs kill -s USR1
+
 warn "-- Applying env inside tmux sessions..."
 IFS=$'\n'
 for line in $(env | grep -e ^TMUX_ -e ^FISH_ -e ^TERM_ | grep -v TMUX_PANE) ; do
@@ -51,10 +55,16 @@ for line in $(env | grep -e ^TMUX_ -e ^FISH_ -e ^TERM_ | grep -v TMUX_PANE) ; do
 		tmux setenv -g "${key}" "${value:-}"
 	fi
 done;
+
 warn "-- Reloading tmux..."
 tmux source-file ~/.tmux.conf
+
 warn "-- Reloading fish..."
 fish -c reload_fish
+
+# move to the end or debug this? https://github.com/gnotclub/xst/blob/1df15396883870cc9dfd19cbddc0a88e81849fb2/src/st.c#L4671
+warn "-- Reloading xst..."
+pgrep "^xst\$" | xargs kill -s USR1
 
 if [[ -f ~/.apply_theme.sh ]] ; then
 	warn "-- Executing '~/.apply_theme.sh' script..."
