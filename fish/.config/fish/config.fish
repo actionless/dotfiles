b5a090e1d3d2ec11d29c14efefdd6ce90967d0e0
actionless <actionless.loveless@gmail.com>
2019-01-22 07:14:29 +0100

    fix(fish: config): don't reset prompt characters if env var isn't set

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,15 @@
 if [ ! -e /tmp/config-"$USER".fish ]
 	echo "Writing config to tmpfs..."
 	cat ~/.config/fish/modules/*.fish > /tmp/config-"$USER".fish
-	set -U -x fish_prompt_left_separator $TERM_DECORATION_LEFT
-	set -U -x fish_prompt_right_separator $TERM_DECORATION_RIGHT
-	set -U -x fish_prompt_mid_separator $TERM_DECORATION_SEPARATOR
+	if test -n "$TERM_DECORATION_LEFT"
+		set -U -x fish_prompt_left_separator $TERM_DECORATION_LEFT
+	end
+	if test -n "$TERM_DECORATION_RIGHT"
+		set -U -x fish_prompt_right_separator $TERM_DECORATION_RIGHT
+	end
+	if test -n "$TERM_DECORATION_SEPARATOR"
+		set -U -x fish_prompt_mid_separator $TERM_DECORATION_SEPARATOR
+	end
 end
 
 source /tmp/config-"$USER".fish
