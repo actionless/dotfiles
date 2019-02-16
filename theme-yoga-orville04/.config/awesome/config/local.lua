fef872259c31803f339565246a8fda8829bb300a
actionless <actionless.loveless@gmail.com>
2019-02-16 05:04:39 +0100

    theme(yoga: orville04): initial commit

---
+++ theme-yoga-orville04/.config/awesome/config/local.lua
@@ -0,0 +1,70 @@
+local awful = require("awful")
+
+
+local local_config = {}
+
+function local_config.init(awesome_context)
+
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
+  awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/twmish/theme.lua"
+
+  --awesome_context.apw_on_the_left= true
+
+  awesome_context.before_config_loaded = function()
+    local beautiful = require('beautiful')
+    local dpi = beautiful.xresources.apply_dpi
+
+    --beautiful.mono_font = "Monospace Bold 9"
+    local font_size = "9.171974522292993"
+    beautiful.bold_font = "Monospace Bold " .. font_size
+    beautiful.mono_font = "Monospace " .. font_size
+    beautiful.sans_font = "Sans Bold " .. font_size
+    beautiful.text_font = "Monospace " .. font_size
+    beautiful.font = beautiful.mono_font
+    beautiful.tasklist_font = beautiful.font
+    beautiful.taglist_font = beautiful.bold_font
+    beautiful.tasklist_font = beautiful.bold_font
+
+    beautiful.menu_height = beautiful.xresources.apply_dpi(28)
+
+    --beautiful.border_radius = beautiful.xresources.apply_dpi(3)
+    --beautiful.panel_widget_border_radius = beautiful.border_radius
+    beautiful.panel_widget_border_width = dpi(2)
+    --beautiful.client_border_radius = beautiful.border_radius * 0.7
+
+    local color_utils = require("actionless.util.color")
+
+    --beautiful.apw_fg_color = beautiful.xrdb.color14
+    --beautiful.apw_bg_color = color_utils.mix(
+      --beautiful.apw_fg_color,
+      --beautiful.panel_bg,
+      --0.3
+    --)
+
+    --beautiful.panel_widget_fg = color_utils.darker(
+      --beautiful.panel_fg,
+      ---13
+    --)
+    --beautiful.panel_widget_bg = color_utils.mix(
+      --beautiful.panel_fg,
+      --beautiful.panel_bg,
+      --0.3
+    --)
+
+    --beautiful.panel_widget_progress_fg = beautiful.xrdb.color14
+    --beautiful.panel_widget_progress_fg = beautiful.panel_widget_fg
+    --beautiful.panel_widget_progress_bg = color_utils.mix(
+      --beautiful.panel_widget_progress_fg,
+      --beautiful.panel_widget_bg,
+      --0.2
+    --)
+    --
+    --
+
+  end
+
+  return awesome_context
+end
+
+return local_config
