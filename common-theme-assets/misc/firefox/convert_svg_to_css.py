623d0bbafe998b1cbc76f75db7f0fb5ce4f39e12
actionless <actionless.loveless@gmail.com>
2019-11-28 16:41:08 +0100

    style(common-theme: firefox): pylint

--- common-theme-assets/misc/firefox/convert_svg_to_css.py
+++ common-theme-assets/misc/firefox/convert_svg_to_css.py
@@ -4,10 +4,16 @@ import sys
 import urllib.parse
 
 
-with open(sys.argv[1]) as f:
-    svg_text = f.read().replace('<?xml version="1.0" encoding="UTF-8"?>', "")
-    encoded_svg = urllib.parse.quote(svg_text)
-    result = 'background-image: url("data:image/svg+xml,{}");'.format(
-        encoded_svg
-    )
-    print(result)
+def convert_svg_to_css(filepath):
+    with open(filepath) as svg_file:
+        svg_text = svg_file.read().replace(
+            '<?xml version="1.0" encoding="UTF-8"?>', ""
+        )
+        encoded_svg = urllib.parse.quote(svg_text)
+        return 'background-image: url("data:image/svg+xml,{}");'.format(
+            encoded_svg
+        )
+
+
+if __name__ == "__main__":
+    print(convert_svg_to_css(sys.argv[1]))
