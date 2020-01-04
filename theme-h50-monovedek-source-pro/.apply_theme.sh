b3c536173c4d9953a59acfdf2f173e25a05536a4
actionless <actionless.loveless@gmail.com>
2020-01-04 13:00:18 +0100

    chore(themes: h50): add 'monovedek-source-pro' theme variant

---
+++ theme-h50-monovedek-source-pro/.apply_theme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+apply-oomoxify -f "Roboto" /opt/oomox/colors/monovedek/monovedek
---
+++ theme-h50-monovedek-source-pro/.config/awesome/config/local.lua
@@ -0,0 +1,41 @@
+local awful = require("awful")
+
+
+local local_config = {}
+
+function local_config.init(awesome_context)
+
+  local conf = awesome_context.config
+  conf.wlan_if = 'wlp4s0'
+  conf.eth_if = 'enp0s25'
+  conf.net_preset = 'systemd'
+  --conf.music_players = { 'spotify', 'clementine', 'mopidy' }
+  --conf.music_players = { 'mopidy', }
+
+  awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/twmish/theme.lua"
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
+
+  awesome_context.autorun = {
+    --"~/.scripts/tp_unmute",
+    --"killall compton ; compton",
+  }
+
+  awesome_context.have_battery = false
+  awesome_context.sensor = "temp1"
+
+  awesome_context.before_config_loaded = function()
+    local beautiful = require("beautiful")
+    --beautiful.font = beautiful.taglist_font
+    --beautiful.bold_font = beautiful.font
+    --beautiful.panel_widget_font = beautiful.font
+    --beautiful.tasklist_font =  beautiful.font
+    --beautiful.titlebar_font =  beautiful.font
+    --beautiful.sans_font = beautiful.font
+  end
+
+  return awesome_context
+end
+
+
+return local_config
