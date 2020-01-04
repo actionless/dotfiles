1103c60028cf1824ca5cf35966695c9d6ccda703
actionless <actionless.loveless@gmail.com>
2020-01-04 12:56:34 +0100

    refactor(themes: h50: retrox/twmish): use newer compton config; use smaller awesome local config; add cursor symlink

--- theme-h50-retrox/.config/awesome/config/local.lua
+++ theme-h50-retrox/.config/awesome/config/local.lua
@@ -5,25 +5,9 @@ local local_config = {}
 
 function local_config.init(awesome_context)
 
-  local conf = awesome_context.config
-  conf.wlan_if = 'wlp4s0'
-  conf.eth_if = 'enp0s25'
-  conf.net_preset = 'systemd'
-  conf.music_players = { 'spotify', 'clementine', 'mopidy' }
-  --conf.music_players = { 'mopidy', }
-
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
   awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/twmish/theme.lua"
-
-  awesome_context.autorun = {
-    --"~/.scripts/tp_unmute",
-    --"killall compton ; compton",
-  }
-
-  awesome_context.have_battery = false
-  awesome_context.sensor = "temp1"
-  awesome_context.new_top = true
 
   return awesome_context
 end
