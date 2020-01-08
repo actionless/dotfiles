848da6fd842929c3add94a14057fab7e77c7f109
actionless <actionless.loveless@gmail.com>
2020-01-08 14:02:56 +0100

    fix(fish: config): don't export prompt-related env vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,29 +4,29 @@ if [ ! -e /tmp/config-"$USER".fish ]
 	echo "Writing config to tmpfs..."
 	cat ~/.config/fish/modules/*.fish > /tmp/config-"$USER".fish
 	if test -n "$TERM_DECORATION_LEFT"
-		set -U -x fish_prompt_left_separator $TERM_DECORATION_LEFT
+		set -U fish_prompt_left_separator $TERM_DECORATION_LEFT
 	else
-		set -U -x fish_prompt_left_separator ""
+		set -U fish_prompt_left_separator ""
 	end
 	if test -n "$TERM_DECORATION_RIGHT"
-		set -U -x fish_prompt_right_separator $TERM_DECORATION_RIGHT
+		set -U fish_prompt_right_separator $TERM_DECORATION_RIGHT
 	else
-		set -U -x fish_prompt_right_separator ""
+		set -U fish_prompt_right_separator ""
 	end
 	if test -n "$TERM_DECORATION_SEPARATOR"
-		set -U -x fish_prompt_mid_separator $TERM_DECORATION_SEPARATOR
+		set -U fish_prompt_mid_separator $TERM_DECORATION_SEPARATOR
 	else
-		set -U -x fish_prompt_mid_separator ""
+		set -U fish_prompt_mid_separator ""
 	end
 	if test -n "$TERM_DECORATION_SEPARATOR_LEFT"
-		set -U -x fish_prompt_mid_separator_left $TERM_DECORATION_SEPARATOR_LEFT
+		set -U fish_prompt_mid_separator_left $TERM_DECORATION_SEPARATOR_LEFT
 	else
-		set -U -x fish_prompt_mid_separator_left ""
+		set -U fish_prompt_mid_separator_left ""
 	end
 	if test -n "$TERM_DECORATION_SEPARATOR_RIGHT"
-		set -U -x fish_prompt_mid_separator_right $TERM_DECORATION_SEPARATOR_RIGHT
+		set -U fish_prompt_mid_separator_right $TERM_DECORATION_SEPARATOR_RIGHT
 	else
-		set -U -x fish_prompt_mid_separator_right ""
+		set -U fish_prompt_mid_separator_right ""
 	end
 end
 
