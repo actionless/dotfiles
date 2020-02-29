87e3c09a864ea0f5d5045690dc7351a231e955ec
actionless <actionless.loveless@gmail.com>
2020-03-01 00:27:29 +0100

    refactor(fish: prompt: git): optimize for big repos and/or with big submodules

--- fish/.config/fish/modules/fish_prompt.fish
+++ fish/.config/fish/modules/fish_prompt.fish
@@ -1,35 +1,30 @@
-function _get_git_branch_name
-	echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _is_git_dirty
-	echo (command git status -s --ignore-submodules=dirty 2>/dev/null)
-end
-
-function _get_git_ahead_count -a branch_name
-	echo (command git log origin/$branch_name..HEAD 2>/dev/null | \
-	grep '^commit' | wc -l | tr -d ' ')
-end
-
-function git_prompt
-	#if not set -q -g __fish_classic_git_functions_defined
-		#set -g __fish_classic_git_functions_defined
-	#end
-	#__fish_git_prompt
-	set git_branch_name (_get_git_branch_name)
-	if [ $git_branch_name ]
-		if [ (_is_git_dirty) ]
-			set_color red -o
-			set git_branch_name_uncommited (git status --porcelain 2>/dev/null| grep "^ [MD]" | wc -l)
-			set git_branch_name_unstaged (git status --porcelain 2>/dev/null| grep "^??" | wc -l)
-			set git_branch_addition " ($git_branch_name_uncommited/$git_branch_name_unstaged)"
-		else
-			if [ (_get_git_ahead_count $git_branch_name) -gt 0 ]
-				set_color green -o
-			end
-		end
-	else
+function _fish_git_prompt
+	set git_branch_name (
+		command git symbolic-ref HEAD 2>/dev/null \
+		| sed -e 's|^refs/heads/||'
+	)
+	if [ -z $git_branch_name ]
 		return 1
+	else
+		set -l IFS ''
+
+		set -l git_status (command git status --porcelain --ignore-submodules=dirty 2>/dev/null)
+		if [ -n "$git_status" ]
+			set_color red -o
+			set git_branch_name_uncommited (echo $git_status | grep "^ [MD]" | wc -l)
+			set git_branch_name_unstaged (echo $git_status | grep "^??" | wc -l)
+			set git_branch_addition " ($git_branch_name_uncommited/$git_branch_name_unstaged)"
+		end
+
+		set git_ahead_count (
+			command git log origin/$git_branch_name..HEAD 2>/dev/null \
+			| grep '^commit' \
+			| wc -l
+		)
+		if [ $git_ahead_count -gt 0 ]
+			set_color green -o
+		end
+
 	end
 	echo " ""$git_branch_name""$git_branch_addition"" "
 end
@@ -85,7 +80,7 @@ $__fish_prompt_hostname " " \
 (set_color -b normal) $fish_prompt_mid_separator \
 $inverse (set_color $fish_color_cwd) " "(pwd -P)" " \
 (set_color -b normal) $fish_prompt_mid_separator \
-$inverse (set_color $fish_color_cwd -o) (git_prompt ; and echo (set_color -b normal)$fish_prompt_mid_separator) \
+$inverse (set_color $fish_color_cwd -o) (_fish_git_prompt ; and echo (set_color -b normal)$fish_prompt_mid_separator) \
 $prompt_status \
 (set_color -b $fish_color_cwd normal) $fish_prompt_mid_separator_right \
 (set_color -b normal $fish_color_cwd) $fish_prompt_right_separator " " \
