caeefca84203798d612a01eaf61d8bf89c3f49ac
actionless <actionless.loveless@gmail.com>
2020-04-20 23:20:17 +0200

    fix(theme: yoga: mnvdk-fntsq: awesome): don't load theme twice

--- theme-yoga-monovedek-fantasque/.config/awesome/config/local.lua
+++ theme-yoga-monovedek-fantasque/.config/awesome/config/local.lua
@@ -6,7 +6,7 @@ local local_config = {}
 function local_config.init(awesome_context)
 
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/lcars-xresources-hidpi/theme.lua"
-  awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
+  --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/gtk/theme.lua"
   awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/pokemon/theme.lua"
   --awesome_context.theme_dir = awful.util.getdir("config") .. "/themes/twmish/theme.lua"
 
