33ff93c898407571e42da2e9f1eb93d5c307163d
actionless <actionless.loveless@gmail.com>
2020-04-20 23:02:20 +0200

    feat(misc: scripts: wayland-launch): add mozilla-related env vars

--- misc/scripts/wayland-launch.sh
+++ misc/scripts/wayland-launch.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-exec env WLC_DRM_DEVICE=card1 QT_QPA_PLATFORM_THEME=wayland-egl GDK_BACKEND=wayland CLUTTER_BACKEND=wayland SDL_VIDEODRIVER=wayland $@
+#    WLC_DRM_DEVICE=card1 \
+exec env \
+	GDK_BACKEND=wayland CLUTTER_BACKEND=wayland \
+	QT_QPA_PLATFORM_THEME=wayland-egl \
+	SDL_VIDEODRIVER=wayland \
+	MOZ_ENABLE_WAYLAND=1 MOZ_DBUS_REMOTE=1 \
+	$@
