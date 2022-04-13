72e45c21399df736b30a72adf89dac3f86bc1d15
actionless <actionless.loveless@gmail.com>
2022-04-13 19:12:13 +0200

    feat(scripts: make_gif): support more output file formats via ffmpeg

--- scripts/scripts/make_gif.bash
+++ scripts/scripts/make_gif.bash
@@ -7,28 +7,43 @@ set -uex
 target=${1-./*.jpg}
 delay=${2-11}
 resize=${3-500}
+output_format=${4:-gif}
 
 workdir=$(mktemp -d)
 cleanup() {
 	echo "Cleaning-up..."
-	rm -r ${workdir}
+	rm -r "$workdir"
 }
 trap cleanup EXIT
 
 current_dir=${PWD}
 
 echo "copying files..."
-cp ${target} ${workdir}/
-cd ${workdir}
-echo "optimizing source images..."
-mogrify -resample 72x72 -resize "${resize}x${resize}" ./*
+cp "$target" "${workdir}/"
+cd "$workdir"
+
+if [[ "$resize" != 0 ]] ; then
+	echo "optimizing source images..."
+	mogrify -resample 72x72 -resize "${resize}x${resize}" ./*
+fi
+
 echo "creating gif..."
-convert -delay ${delay} -loop 0 ./* myimage.gif
-echo "optimizing gif..."
-gifsicle --batch --optimize=3 --colors 256 myimage.gif;
-echo "cleaning-up..."
-mv myimage.gif ${current_dir}/
-cd ${current_dir}
+convert -delay "$delay" -loop 0 ./* myimage.gif
+
+if [[ "$output_format" == "gif" ]] ; then
+	echo "optimizing gif..."
+	gifsicle --batch --optimize=3 --colors 256 myimage.gif;
+fi
+
+echo "moving result to the current directory..."
+mv myimage.gif "${current_dir}/"
+cd "$current_dir"
+if [[ "$output_format" != "gif" ]] ; then
+	echo "converting to ${output_format}..."
+	ffmpeg -i ./myimage.gif "./myimage.${output_format}"
+	rm ./myimage.gif
+fi
+
 echo "DONE"
-ls -lh ./myimage.gif
-viewnior ./myimage.gif
+
+mpvn --loop "./myimage.${output_format}"
