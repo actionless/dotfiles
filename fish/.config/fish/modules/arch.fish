2b2dda1c76e5688863463545b2d3a71102f8a21e
actionless <actionless.loveless@gmail.com>
2022-11-05 00:01:53 +0100

    chore(fish: modules: arch): move systemd-specific to a separate module

--- fish/.config/fish/modules/arch.fish
+++ fish/.config/fish/modules/arch.fish
@@ -1,9 +1,3 @@
-abbr sc 'sudo systemctl'
-
-#abbr y 'yaourt'
-#abbr p 'pacaur'
-#abbr t 'trizen'
-#abbr y 'yay'
 abbr p 'pikaur'
 abbr syua 'pikaur -Syu'
 abbr syu  'pikaur -Syu'
---
+++ fish/.config/fish/modules/systemd.fish
@@ -0,0 +1,1 @@
+abbr sc 'sudo systemctl'
