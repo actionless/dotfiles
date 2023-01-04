37b3b165fe997b37723e82d0099dc91a8955ba84
actionless <actionless.loveless@gmail.com>
2023-01-04 23:33:40 +0100

    feat(fish: debian: pikaur): add list and query

--- fish/.config/fish/available_modules/debian.fish
+++ fish/.config/fish/available_modules/debian.fish
@@ -8,18 +8,26 @@ function p -a cmd -d 'pikaur placeholder'
 	if test (count $argv) -gt 1;
 		set rest_argv (_cdr $argv)
 	end
-	argparse --name=pikaur 'u/sysupgrade' 'y/refresh' 'S/sync' -- $argv
+	argparse --name=pikaur 'u/sysupgrade' 'y/refresh' 'S/sync' 'Q/query' 'l/list' -- $argv
 	if test $status -gt 0
 		return $status
 	end
-	if test -n $_flag_sync
-		if test -n $_flag_sync
+	if test -n "$_flag_sync"
+		if test -n "$_flag_refresh"
 			sudo apt update
 		end
-		if test -n $_flag_sysupgrade
+		if test -n "$_flag_sysupgrade"
 			sudo apt dist-upgrade
 		end
 		sudo apt install $rest_argv
+	else if test -n "$_flag_query"
+		if test -n "$_flag_list"
+			sudo dpkg -L $rest_argv
+		else
+			echo "Query: Unknown args: $argv"
+		end
+	else
+		echo "Unknown args: $argv"
 	end
 	return $status
 end
