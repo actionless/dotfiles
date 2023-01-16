b88c591725fb5165bcde4e4c92058607c6c4e69e
actionless <actionless.loveless@gmail.com>
2023-01-17 00:11:19 +0100

    feat(fish): add !! and !![1-9]+ abbreviations [new 3.6.0 rules 🔥]

--- fish/.config/fish/modules/bash.fish
+++ fish/.config/fish/modules/bash.fish
@@ -13,3 +13,13 @@ end
 function watch --description 'Wrapper for watch'
 	command watch --interval=1 -c -d "$FISH -c '$argv'"
 end
+
+function last_history_item; echo $history[1]; end
+abbr -a !! --position anywhere --function last_history_item
+
+function _last_history_item_nth_word;
+	set -l cmd (string split " " $history[1])
+	set -l idx -(echo $argv[1] | string split '!')[3]
+	echo $cmd[$idx]
+end
+abbr -a last_history_item_nth_word_abbr --position anywhere --regex "!![0-9]+" --function _last_history_item_nth_word
