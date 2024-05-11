3625844ff0715eaa26aaae80aac90869576d150a
actionless <actionless.loveless@gmail.com>
2024-05-11 02:12:43 +0200

    chore(scripts: cleanup-root): disable some items and prune only dangling docker stuff

--- scripts/scripts/cleanup_root.sh
+++ scripts/scripts/cleanup_root.sh
@@ -46,37 +46,49 @@ fi
 #docker container prune -f || true
 #docker image prune -f || true
 #docker system prune -f || true
-docker system prune -af --volumes || true
+#docker system prune -af --volumes || true
+docker system prune -f --volumes || true
 if [[ ${docker_status} -ne 0 ]] ; then
 	sudo systemctl stop docker || true
 fi
 
 
-pikaur -Scc --repo --noconfirm
+#pikaur -Scc --repo --noconfirm
 echo
 echo
 
 
 games_dest_dir="/opt/games"
 
-opt_games_dir="/opt"
-opt_games_list=(
-	'Pokemon Revolution'
-	'teamviewer'
-	'urbanterror'
-	'warsow'
-	'thedarkmod'
-)
-relocate_games "${games_dest_dir}" "${opt_games_dir}" "${opt_games_list[@]}"
+#opt_games_dir="/opt"
+#opt_games_list=(
+#    'Pokemon Revolution'
+#    'teamviewer'
+#    'urbanterror'
+#    'warsow'
+#    'thedarkmod'
+#    'GuitarPro6'
+#    'bitwig-studio.bak'
+#    'oomox'
+#)
+#relocate_games "${games_dest_dir}" "${opt_games_dir}" "${opt_games_list[@]}"
 
-usr_share_games_dir="/usr/share"
-usr_share_games_list=(
-	'xonotic'
-	'supertuxkart'
-)
-relocate_games "${games_dest_dir}" "${usr_share_games_dir}" "${usr_share_games_list[@]}"
+#games_dest_dir="/media/old_media/ai/system"
 
-rm -fr /opt/user_cache/spotify/Data/*
+#opt_games_dir="/opt"
+#opt_games_list=(
+#    'cuda'
+#)
+#relocate_games "${games_dest_dir}" "${opt_games_dir}" "${opt_games_list[@]}"
+
+#usr_share_games_dir="/usr/share"
+#usr_share_games_list=(
+#    'xonotic'
+#    'supertuxkart'
+#)
+#relocate_games "${games_dest_dir}" "${usr_share_games_dir}" "${usr_share_games_list[@]}"
+
+#rm -fr /opt/user_cache/spotify/Data/*
 
 echo
 echo
