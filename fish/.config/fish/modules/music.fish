fbaba193eb2b7ea6c238809422431be09b687136
actionless <actionless.loveless@gmail.com>
2024-05-29 16:24:00 +0200

    feat(fish: music): add meters for avantree

--- fish/.config/fish/modules/music.fish
+++ fish/.config/fish/modules/music.fish
@@ -7,6 +7,7 @@
 
 set -l m50 "ATH-M50xBT    "
 set -l bose "Bose AE2 SoundLink"
+set -l avantree "Avantree C81 Analog Stereo"
 set -l LEFT "monitor_FL"
 set -l RIGHT "monitor_FR"
 set -l PPM_GAIN 0
@@ -31,3 +32,9 @@ abbr meter_m50_ebu "ebumeter -cl '$m50:$LEFT' -cr '$m50:$RIGHT'"
 
 #abbr meter_m50_ppm-5db "meterbridge '$m50:$LEFT' '$m50:$RIGHT' -t ppm -r (math $PPM_GAIN - 5)"
 #abbr meter_m50_vu-5db  "meterbridge '$m50:$LEFT' '$m50:$RIGHT' -t vu -r +(math $VU_GAIN - 5)"
+
+
+abbr meter_avan_ppm "meterbridge  '$avantree:$LEFT'     '$avantree:$RIGHT' -t ppm -r $PPM_GAIN"
+abbr meter_avan_vu  "meterbridge  '$avantree:$LEFT'     '$avantree:$RIGHT' -t vu -r +$VU_GAIN"
+abbr meter_avan_jf  "meterbridge  '$avantree:$LEFT'     '$avantree:$RIGHT' -t jf"
+abbr meter_avan_ebu "ebumeter -cl '$avantree:$LEFT' -cr '$avantree:$RIGHT'"
