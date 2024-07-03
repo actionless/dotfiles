f61f6481813a5c5cd131426240498eac493a83c9
actionless <actionless.loveless@gmail.com>
2024-07-03 09:39:13 +0200

    chore(fish: arch: abbr): add syua and downgrade

--- fish/.config/fish/modules/arch.fish
+++ fish/.config/fish/modules/arch.fish
@@ -1,5 +1,7 @@
 abbr p 'pikaur'
-abbr syua 'pikaur -Syu'
 abbr syu  'pikaur -Syu'
+abbr syua 'pikaur -Syua'
 
 alias mksrcinfo 'makepkg --printsrcinfo > .SRCINFO'
+
+abbr downgrade 'sudo downgrade --ignore never '
