91a06239a207fc6e7b56bf0088184d4cf4cc1118
actionless <actionless.loveless@gmail.com>
2024-07-12 08:30:44 +0200

    style(_scripts: etc-diff): indent

--- _scripts/etc-diff.sh
+++ _scripts/etc-diff.sh
@@ -42,52 +42,52 @@ purple() {
 
 for SYSTEM_CONFIGS_PATH in "${SYSTEM_CONFIGS_PATHS[@]}" ; do
 
-REFERENCE_CONFIGS_PATH="${HOME}${DOTFILES_SUBDIR}${SYSTEM_CONFIGS_PATH}"
+	REFERENCE_CONFIGS_PATH="${HOME}${DOTFILES_SUBDIR}${SYSTEM_CONFIGS_PATH}"
 
-configs_paths=()
-while IFS= read -r -d '' reference_config_path ; do
-	configs_paths+=("$reference_config_path")
-done < <(find -L "$REFERENCE_CONFIGS_PATH" -type f -print0)
-#echo "::: ${configs_paths[*]}"
+	configs_paths=()
+	while IFS= read -r -d '' reference_config_path ; do
+		configs_paths+=("$reference_config_path")
+	done < <(find -L "$REFERENCE_CONFIGS_PATH" -type f -print0)
+	#echo "::: ${configs_paths[*]}"
 
-for reference_config_path in "${configs_paths[@]}" ; do
-	#echo ":: $reference_config_path"
-	system_config_path=${reference_config_path//$REFERENCE_CONFIGS_PATH/$SYSTEM_CONFIGS_PATH}
+	for reference_config_path in "${configs_paths[@]}" ; do
+		#echo ":: $reference_config_path"
+		system_config_path=${reference_config_path//$REFERENCE_CONFIGS_PATH/$SYSTEM_CONFIGS_PATH}
 
 		dotfile_relpath=$(readlink -e "$reference_config_path" | sed -e "s|$DOTFILES_PATH/||")
 		if [[ -n "$filter" ]] && ! grep "$filter" > /dev/null <<< "$dotfile_relpath" ; then
 			continue
 		fi
 
-	diff -q "$reference_config_path" "$system_config_path" > /dev/null || {
-		if [[ $names_only -eq 0 ]] ; then
-			yellow -----------------------------------------------------------------
-			echo "$(purple :: "${dotfile_relpath%%/*}")/${dotfile_relpath#*/}"
-			yellow -----------------------------------------------------------------
-			diff --color=always -u "$reference_config_path" "$system_config_path"
-			echo
-		else
-			echo "$dotfile_relpath"
-		fi
-		if [[ "$interactive" -eq 1 ]] ; then
-			echo "[u] update"
-			echo "[return] skip"
-			read -r answer
-			if [[ "$answer" = "u" ]] ; then
-				set -x
-				cp "$system_config_path" "$dotfile_relpath"
-				set +x
-				echo "[g] git add"
+		diff -q "$reference_config_path" "$system_config_path" > /dev/null || {
+			if [[ $names_only -eq 0 ]] ; then
+				yellow -----------------------------------------------------------------
+				echo "$(purple :: "${dotfile_relpath%%/*}")/${dotfile_relpath#*/}"
+				yellow -----------------------------------------------------------------
+				diff --color=always -u "$reference_config_path" "$system_config_path"
+				echo
+			else
+				echo "$dotfile_relpath"
+			fi
+			if [[ "$interactive" -eq 1 ]] ; then
+				echo "[u] update"
 				echo "[return] skip"
 				read -r answer
-				if [[ "$answer" = "g" ]] ; then
+				if [[ "$answer" = "u" ]] ; then
 					set -x
-					git add "$dotfile_relpath"
+					cp "$system_config_path" "$dotfile_relpath"
 					set +x
+					echo "[g] git add"
+					echo "[return] skip"
+					read -r answer
+					if [[ "$answer" = "g" ]] ; then
+						set -x
+						git add "$dotfile_relpath"
+						set +x
+					fi
 				fi
 			fi
-		fi
-	}
-done
+		}
+	done
 
 done
