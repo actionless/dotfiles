c26eb0348b88cfde58501cc5e005cc25e4507efb
actionless <actionless.loveless@gmail.com>
2024-09-08 00:01:32 +0200

    feat(fish: arch): use pikaur as one of command-not-found handlers

--- fish/.config/fish/modules/arch.fish
+++ fish/.config/fish/modules/arch.fish
@@ -5,3 +5,9 @@ abbr syua 'pikaur -Syua'
 alias mksrcinfo 'makepkg --printsrcinfo > .SRCINFO'
 
 abbr downgrade 'sudo downgrade --ignore never '
+
+#if not status is-interactive
+	function fish_command_not_found --on-event fish_command_not_found
+		pkgfile $argv[1] ; or pikaur -Ss $argv[1]
+	end
+#end
