f705cafac79659b7e229a1e8ba62a8503161ce9d
actionless <actionless.loveless@gmail.com>
2024-09-07 23:59:31 +0200

    refactor(fish: lib: cdr): simplify it a bit

--- fish/.config/fish/modules/10-lib.fish
+++ fish/.config/fish/modules/10-lib.fish
@@ -1,9 +1,7 @@
 function _cdr -d "CDR, CAR's friend"
-	set -f new_argv
 	if test (count $argv) -gt 1;
-		set -f new_argv $argv[2..(count $argv)]
-	end
-	for arg in $new_argv
-		echo $arg
+		for arg in $argv[2..(count $argv)]
+			echo $arg
+		end
 	end
 end
