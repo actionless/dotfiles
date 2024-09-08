09cbba46bbf6e85bdfd60f23b2ef9970cc551479
actionless <actionless.loveless@gmail.com>
2024-09-08 08:07:04 +0200

    fix(fish: arch: command_not_found): enable only in interactive shell, not in the scripts

--- fish/.config/fish/modules/arch.fish
+++ fish/.config/fish/modules/arch.fish
@@ -6,8 +6,8 @@ alias mksrcinfo 'makepkg --printsrcinfo > .SRCINFO'
 
 abbr downgrade 'sudo downgrade --ignore never '
 
-#if not status is-interactive
+if status is-interactive
 	function fish_command_not_found --on-event fish_command_not_found
 		pkgfile $argv[1] ; or pikaur -Ss $argv[1]
 	end
-#end
+end
