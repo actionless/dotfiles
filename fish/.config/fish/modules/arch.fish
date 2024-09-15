4078969367ed2f29abeef34ff873aa5835d41fec
actionless <actionless.loveless@gmail.com>
2024-09-15 02:10:59 +0200

    chore(fish: arch: command_not_found): ask confirmation before doing internet lookup

--- fish/.config/fish/modules/arch.fish
+++ fish/.config/fish/modules/arch.fish
@@ -8,6 +8,10 @@ abbr downgrade 'sudo downgrade --ignore never '
 
 if status is-interactive
 	function fish_command_not_found --on-event fish_command_not_found
-		pkgfile $argv[1] ; or pikaur -Ss $argv[1]
+		pkgfile $argv[1] ; or begin
+			echo ":: PkgFile: '$argv[1]' not found"
+			read -f search_aur -P ':: search with pikaur? [y/N] > '
+			string match -- "y*" "$search_aur" ; and pikaur -Ss $argv[1]
+		end
 	end
 end
