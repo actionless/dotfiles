414303de1b8defdc01b1a74d5a19d1c3b499cfe3
actionless <actionless.loveless@gmail.com>
2024-11-07 05:18:58 +0100

    feat(fish: lib): add random_word() function

--- fish/.config/fish/modules/10-lib.fish
+++ fish/.config/fish/modules/10-lib.fish
@@ -15,6 +15,18 @@ function _cdr -d "CDR, CAR's friend"
 	end
 end
 
+function _random_word
+	set -f dict /usr/share/dict/words
+	cat $dict \
+	| head -n (
+		random 1 (
+			wc -l $dict \
+			| cut -d' ' -f1
+		)
+	) \
+	| tail -n 1
+end
+
 function _capitalize
 	string replace -r -a '\b([\w])' '\U$0' $argv
 end
