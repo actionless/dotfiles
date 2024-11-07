2dccecc3006849e31f9a850c9ee13274625f4fbe
actionless <actionless.loveless@gmail.com>
2024-11-07 05:21:35 +0100

    feat(fish: arch: command_not_found): ignore accidental arrow keys

--- fish/.config/fish/modules/arch.fish
+++ fish/.config/fish/modules/arch.fish
@@ -7,8 +7,15 @@ alias mksrcinfo 'makepkg --printsrcinfo > .SRCINFO'
 abbr downgrade 'sudo downgrade --ignore never '
 
 if status is-interactive
+	set -g __ignore_notfound_cmds '[1' '2A' '2A[1' '5A' '5B'
+
 	function fish_command_not_found --on-event fish_command_not_found
-		pkgfile $argv[1] ; or begin
+		set -f cmd $argv[1]
+		if contains $cmd $__ignore_notfound_cmds
+			forshtune | pikasay --orientation vertical
+			return 1
+		end
+		pkgfile "$cmd" ; or begin
 			echo ":: PkgFile: '$argv[1]' not found"
 			read -f search_aur -P ':: search with pikaur? [y/N] > '
 			string match -- "y*" "$search_aur" ; and pikaur -Ss $argv[1]
