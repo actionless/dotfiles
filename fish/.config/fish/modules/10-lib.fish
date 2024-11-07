4f920adfdf1508530403c51a253b8a22592cf718
actionless <actionless.loveless@gmail.com>
2024-11-07 05:14:07 +0100

    feat(fish: lib): add car() function

--- fish/.config/fish/modules/10-lib.fish
+++ fish/.config/fish/modules/10-lib.fish
@@ -1,3 +1,12 @@
+function _car
+	if test (count $argv) -gt 1;
+		for arg in $argv[1..(math (count $argv) - 1)]
+			echo $arg
+		end
+	end
+end
+
+
 function _cdr -d "CDR, CAR's friend"
 	if test (count $argv) -gt 1;
 		for arg in $argv[2..(count $argv)]
